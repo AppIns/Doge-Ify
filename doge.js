465ec3e16ced6bd39208f423793b5af0c1b9ca2a
AppIns <AlexAndersonOne@gmail.com>
2015-10-20 22:36:37 -0700

    v0.1.1
    more images

--- doge.js
+++ doge.js
@@ -16,7 +16,11 @@ var imgsrc = ["https://c2.staticflickr.com/4/3689/11025069063_4132db62d2_b.jpg",
 "https://c2.staticflickr.com/4/3721/12139928374_d16d9fd29d.jpg",
 "https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg",
 "https://upload.wikimedia.org/wikipedia/commons/7/78/Akita_inu.jpeg",
-"https://upload.wikimedia.org/wikipedia/commons/6/6b/Taka_Shiba.jpg"];
+"https://upload.wikimedia.org/wikipedia/commons/6/6b/Taka_Shiba.jpg",
+"https://upload.wikimedia.org/wikipedia/commons/2/20/Shiba_Inu.jpg",
+"http://img01.deviantart.net/e7c4/i/2014/161/8/e/shiba_inu_13_by_xxtgxxstock-d7lvpf4.jpg",
+"http://img13.deviantart.net/6f0a/i/2014/141/9/0/shiba_inu_by_xxtgxxstock-d7j6ozc.jpg",
+"https://c2.staticflickr.com/6/5219/5504186832_ae53584120.jpg"];
 
 // Amount of images
 var amt = document.getElementsByTagName("IMG").length;
